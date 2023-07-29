6856077d247295d30d56af46170d544e9dffee73
Joshua Vaneps <joshuavaneps@yahoo.com>
2023-07-29 15:21:32 -0400

    quick edit to homeroute to fix error

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -1,48 +1,48 @@
-const router = require('express').Router();
-const { Mezcal, User } = require('../models');
-const withAuth = require('../utlities/auth');
+const router = require("express").Router();
+const { Mezcal, User } = require("../models");
+const withAuth = require("../utlities/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all mezcals and JOIN with user data
     const mezcalData = await Mezcal.findAll({
-      // include: [
-      //   {
-      //     model: Mezcal,
-      //     attributes: ['name'],
-      //   },
-      // ],
+      include: [
+        {
+          model: Mezcal,
+          attributes: ["name"],
+        },
+      ],
     });
 
     // Serialize data so the template can read it
-    const mezcals = mezcalData.map((Mezcal) => Mezcal.get({ plain: true }));
+    const mezcals = mezcalData.map((mezcal) => mezcal.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      mezcals, 
-      // logged_in: req.session.logged_in 
+    res.render("homepage", {
+      mezcals,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('models/Mezcal/:id', withAuth, async (req, res) => {
+router.get("models/Mezcal/:id", withAuth, async (req, res) => {
   try {
     const mezcalData = await Mezcal.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
 
     const mezcal = mezcalData.get({ plain: true });
 
-    res.render('mezcal', {
+    res.render("mezcal", {
       ...mezcal,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -50,33 +50,33 @@ router.get('models/Mezcal/:id', withAuth, async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Mezcal }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render("profile", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
