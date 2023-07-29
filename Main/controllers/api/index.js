8c9097c0d01b5ea836ad7d953c407337700cbd49
mandaark17 <alec.gomez417@gmail.com>
2023-07-29 14:12:02 -0400

    adjusted homeroutes.js params

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const mezcalRoutes = require('./mezcalRoutes');
 
-router.use('/api', userRoutes);
+router.use('/login', userRoutes);
 router.use('/mezcal', mezcalRoutes);
 
 module.exports = router;
