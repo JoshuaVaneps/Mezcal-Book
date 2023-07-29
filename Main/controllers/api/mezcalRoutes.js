014aa5c6a039035cc9c223d192eb9bf51918efc4
mandaark17 <alec.gomez417@gmail.com>
2023-07-29 13:22:47 -0400

    mezcalRoutes.js should be done

--- Main/controllers/api/mezcalRoutes.js
+++ Main/controllers/api/mezcalRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Mezcal } = require('../../models');
+const Mezcal = require('../../models/Mezcal');
 const withAuth = require('../../utlities/auth');
 
 router.post('/addMezcal', withAuth, async (req, res) => {
@@ -14,6 +14,27 @@ router.post('/addMezcal', withAuth, async (req, res) => {
     res.status(200).json(newMezcal);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const dish = await Mezcal.update(
+      {
+        name: req.body.name,
+        agave: req.body.agave,
+        abv: req.body.abv,
+        isManager: req.body.isManager,
+      },
+      // {
+      //   where: {
+      //     id: req.params.id,
+      //   },
+      // }
+    );
+    res.status(200).json(dish);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
