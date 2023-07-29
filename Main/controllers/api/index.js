f749fcfbb52a0f36c5128af9be70c6d4c7f540a0
mandaark17 <alec.gomez417@gmail.com>
2023-07-29 13:54:05 -0400

    middlewear

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const mezcalRoutes = require('./mezcalRoutes');
 
-router.use('/users', userRoutes);
+router.use('/api', userRoutes);
 router.use('/mezcal', mezcalRoutes);
 
 module.exports = router;
