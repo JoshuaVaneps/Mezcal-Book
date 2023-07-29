894a6be854ef27860580fb3e26dcda5d1f2d73a7
mandaark17 <alec.gomez417@gmail.com>
2023-07-29 13:44:17 -0400

    fixed routes for controllers

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const mezcalRoutes = require('./mezcalRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/mezcal', mezcalRoutes);
 
 module.exports = router;
