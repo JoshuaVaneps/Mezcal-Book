5b64be76c90f739aa861830089dd6c950ffc7562
Nick <nicksweetj@gmail.com>
2023-08-15 02:40:39 -0400

    added a FUNCTIONING search bar. Still need to implement search by agave type, age, and abv. Also, the search bar will function if the user is not logged in

--- public/js/search.js
+++ public/js/search.js
@@ -1,24 +1,18 @@
-var searchForm = document.getElementById("search-form"); // Corrected ID name
-var searchInput = document.getElementById('search-input');
+var searchQuery = document.getElementById("search-form");
+const searchResultsContainer = document.getElementById("search-results");
 
-searchForm.addEventListener('submit', function(event) { // Corrected parentheses
-  event.preventDefault(); // Prevent form submission
+function searchForm(event) {
+  event.preventDefault();
+  const userSearch = document
+    .getElementById("search-input")
+    .value.toLowerCase();
 
-  const searchInput = document.getElementById('search-input').value.toLowerCase();
+  // Construct the URL with query parameters
+  const url = `/views?search=${encodeURIComponent(userSearch)}`;
 
-  
-  fetch('mezcals.json')
-    .then(response => response.json())
-    .then(data => {
-      const foundMezcal = data.find(mezcal => mezcal.name.toLowerCase() === searchInput);
-      if (foundMezcal) {
-        document.getElementById('result').textContent = `Name: ${foundMezcal.name}, Agave: ${foundMezcal.agave}, ABV: ${foundMezcal.abv}, Origin: ${foundMezcal.origin}, Aroma: ${foundMezcal.aroma}, Pallet: ${foundMezcal.pallet}`;
-      } else {
-        document.getElementById('result').textContent = 'Mezcal not found';
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-});
+  console.log(searchQuery);
+  // Redirect to the search route
+  window.location.href = url;
+}
 
+searchQuery.addEventListener("submit", searchForm);
